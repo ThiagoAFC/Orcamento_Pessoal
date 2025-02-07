d09548cb750f7ae0f833c3ca1fb2f8bacb823f6f
Thiago Oliveira <thiago@hsbconsultoria.com.br>
2025-02-07 06:56:22 -0300

    chore: Risk Analysis with OWASP

--- Tests/obj/Debug/net8.0/Sistema_Bancario.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Sistema_Bancario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema_Bancario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8826bbcc6ac0c9b1d0fcf9fee0351c9ff533a5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91624558e804d1cbb0e8fcba162c406303dada4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema_Bancario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema_Bancario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
